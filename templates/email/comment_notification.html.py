6314d6558100f9ec0d04746aad8ae56ac3d08777
aji <aji@ajide-MacBook-Pro.local>
2017-02-19 19:18:16 +0800

    first commit

---
+++ templates/email/comment_notification.html.py
@@ -0,0 +1,17 @@
+BBBBBBB BBBBBBBBBBBBBBBBB
+BBBB BBBB
+
+BBBBB BBBB
+XXX
+     gettext('A new comment has been posted on') 
+    XX XXXXXXXXXXXXXXXXXXX
+XXXX
+XXX
+     gettext('\n    At %(submit_date)s %(user_name)s wrote:\n    ') 
+    SS SSSSSSSSSSSSSSS SSSSSSSSSSSSS SSSSSS
+    
+XXXX
+XXX
+    FFFFFFFFFFFFFFFFFF
+XXXX
+BBBBBBBB
