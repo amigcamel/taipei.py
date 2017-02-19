057d4fa584a705eb8f5026679ce988ef5d42ab50
aji <aji@ajide-MacBook-Pro.local>
2017-02-19 19:34:30 +0800

    add taipei-py.herokuapp.com into ALLOWED_HOSTS

--- taipei_py/settings.py
+++ taipei_py/settings.py
@@ -93,7 +93,8 @@ USE_MODELTRANSLATION = False
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = [
-    'localhost',        
+    'localhost',
+    'taipei-py.herokuapp.com',
 ]
 
 # Local time zone for this installation. Choices can be found here:
